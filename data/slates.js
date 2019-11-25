0339243fc34fd9a2e89427326521b7efed76e6ea
Alex <avance@bulldogcreative.com>
2019-11-25 16:03:10 -0500

    new slates

--- data/slates.js
+++ data/slates.js
@@ -1,37 +1,42 @@
 let collegeSlates = [
 
 	{
-		name: "11/23 Saturday Main",
-		players: collegeMainPlayers,
+		name: "11/27 Tuesday MACtion",
+		players: collegeTuesdayPlayers,
 		hasOwnership: false
 	},
 	{
-		name: "11/23 Saturday Night",
-		players: collegeNightPlayers,
+		name: "11/29 Friday",
+		players: collegeFridayPlayers,
 		hasOwnership: false
-	},
-	{
-		name: "11/23 Saturday Late Night",
-		players: collegeLateNightPlayers,
-		hasOwnership: false	
 	}
+	// {
+	// 	name: "11/30 Saturday Main",
+	// 	players: collegeMainPlayers,
+	// 	hasOwnership: false
+	// }
+	// {
+	// 	name: "11/23 Saturday Late Night",
+	// 	players: collegeLateNightPlayers,
+	// 	hasOwnership: false	
+	// }
 ];
 
 let proSlates = [
 
 	{
-		name: "11/24 Main",
+		name: "11/28 Thanksgiving Main",
+		players: proThursdayPlayers,
+		hasOwnership: false
+	},
+	// {
+	// 	name: "12/1 Early Only",
+	// 	players: proEarlyPlayers,
+	// 	hasOwnership: false
+	// },
+	{
+		name: "12/1 Main Slate",
 		players: proMainPlayers,
-		hasOwnership: false
-	},
-	{
-		name: "11/24 Early Only",
-		players: proEarlyPlayers,
-		hasOwnership: false
-	},
-	{
-		name: "11/24 Afternoon Only",
-		players: proAfternoonPlayers,
 		hasOwnership: false
 	}
 
